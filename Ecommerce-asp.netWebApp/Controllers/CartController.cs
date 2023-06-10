f3ed8d8a785f6052e9066d8cc8eb01f9cc90d17d
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-06-10 14:38:25 +0300

    cart operations

---
+++ Ecommerce-asp.netWebApp/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using Ecommerce_asp.netWebApp.Entity;
+using Ecommerce_asp.netWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce_asp.netWebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+        public PartialViewResult Summary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
